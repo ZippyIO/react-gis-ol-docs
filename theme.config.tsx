0a63b0804fbab603c355ec5fba80c7653d6ff137
Nick <delta2888@gmail.com>
2024-02-19 11:32:15 +1100

    build: :wrench: update package.json & nextra theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  useNextSeoProps: () => {
+    return {
+      titleTemplate: "%s | React GIS-OL",
+    };
+  },
+  logo: <span>React GIS-OL</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/ZippyIO/react-gis-ol",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "React GIS-OL Documentation",
   },
-}
+};
 
-export default config
+export default config;
